24d668c918b877f84a2ce8913587260f973afaae
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-24 10:45:43 +0530

    conditions&special_var-desc

--- logs.sh
+++ logs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)
-echo "$USERID"
+userid=$(id -u)
+
+
+if [ $userid -eq 0 ]
+then
+    echo "You are super user"
+else
+    echo "Please run this file with super user privileges"
+    exit 1
+fi
