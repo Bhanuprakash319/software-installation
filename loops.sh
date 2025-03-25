9275cc8d62ee129aecd2ac17a9e5282be29faa87
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-25 23:45:18 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 userid=$(id -u)
+time=$(date +%F-%H-%M-%S)
+filename=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$time-$filename.log
 
 r="\e[31m"
 g="\e[32m"
@@ -14,11 +17,23 @@ else
     exit 1
 fi
 
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "installation of $2 is $r failure $n"
+        exit 1
+    else
+         echo -e "installation of $2 is $g success $n"
+}
 
-for i in {1..20}
+dnf install mysql -y &>>$logfile
+validate $? "mysql"
+
+
+'for i in {1..20}
 do 
    echo "$i"
-done
+done'
 
 
 
