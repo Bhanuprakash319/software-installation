12ce1f3295ff3c583ed8ae12dc50a1c039ca8ed3
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-25 22:58:44 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 userid=$(id -u)
-#time=$(date +%F-%H-%M-%S)
-#scriptname=$(echo $0 | cut -d "." -f1)
-#logfile=/tmp/$scriptname-$time.log'
+time=$(date +%F-%H-%M-%S)
+scriptname=$(echo $0 | cut -d "." -f1)
+logfile=/tmp/$scriptname-$time.log
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+#TIMESTAMP=$(date +%F-%H-%M-%S)
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+#LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
 if [ $userid -eq 0 ]
