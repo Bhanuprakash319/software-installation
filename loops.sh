c8007ab4f5baddbf0307bc53277dd44ba912c738
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-25 23:36:54 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+userid=$(id -u)
+
+r="\e[31m"
+g="\e[32m"
+n="\e[0m"
+
+if [ $userid -eq 0 ]
+then
+    echo "$g You are super user $n"
+else
+    echo "$r You are not super user $n"
+    exit 1
+fi
+
+
 for i in {1..20}
 do 
    echo "$i"
 done
+
+
+
