e2a7aa3f2b67cf0d7d4d7a5322c563a2deb901d3
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-25 23:26:35 +0530

    logs

--- logs.sh
+++ logs.sh
@@ -4,6 +4,9 @@ userid=$(id -u)
 time=$(date +%F-%H-%M-%S)
 scriptname=$(echo $0 | cut -d "." -f1)
 logfile=/tmp/$scriptname-$time.log
+r="\e[31m"
+g="\e[32m"
+n="\e[0m"
 
 #TIMESTAMP=$(date +%F-%H-%M-%S)
 #SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -21,10 +24,12 @@ fi
 validation(){
     if [ $1 -ne 0 ]
     then 
-        echo -e "\e[31m Installation of $2 is failure"
+        echo -e "Installation of $2 is $rfailure"
+        echo "hi"
         exit 1
     else
-        echo -e "\e[32m Installation \e[0m of $2 is success"
+        echo -e "Installation of $2 is $gsuccess"
+        echo "hi"
     fi
 }
 
