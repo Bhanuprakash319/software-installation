a218b53b468ca0a1f20cd2ad6344093f859ab185
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-25 11:49:14 +0530

    installation of nginx

--- nginx.sh
+++ nginx.sh
@@ -6,4 +6,25 @@ then
     echo "you are super user"
 else
     echo "you are not super user"
+    exit 1
 fi
+
+validate()
+{
+    if [ $1 -eq 0 ]
+    then 
+         echo "$2 is successful"
+    else
+         echo "$2 is failure"
+         exit 1 
+}
+
+
+dnf install nginx -y
+validate $? "installing nginx"
+
+systemctl enable nginx
+validate $? "installing nginx"
+
+systemctl start nginx
+validate $? "starting nginx"
