5c62c8557fd6b14339097bc6ed24cf96a67c3ac6
Bhanuprakash319 <puthabhanu@gmail.com>
2025-03-29 09:19:58 +0530

    script-calling

--- source1.sh
+++ source1.sh
@@ -6,6 +6,6 @@ g="\e[32m"
 n="\e[0m"
 
 a="bhanu"
-echo -e "Process id of source1 is$g $$$n"
+echo -e "Process id of source1 is$g $$ $n"
 ./source2.sh
 echo -e "Hi i am $g bhanu$n"
--- source2.sh
+++ source2.sh
@@ -5,5 +5,5 @@ g="\e[32m"
 n="\e[0m"
 
 a="siva"
-echo -e "Process id of source 2 is$g $$$n"
+echo -e "Process id of source 2 is$g $$ $n"
 echo -e "Hi i am $g bhanu$n"
